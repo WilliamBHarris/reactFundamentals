fafef6ea7d5877f32844df089ff51061351a704c
Will Harris <bharris010486@gmail.com>
2021-11-09 15:34:42 -0500

    to mod 5.4

---
+++ src/components/site/Sidebar.js
@@ -0,0 +1,63 @@
+import { Route, Link, Switch } from "react-router-dom";
+import Home from "./Home";
+import Resources from "./Resources";
+import FunctionalComponentDemo from "../concepts/FunctionalComponents/FunctionalComponentDemo";
+import JSXRules from "../concepts/FunctionalComponents/JSXRules";
+import State from "../concepts/FunctionalComponents/State";
+import Effects from "../concepts/FunctionalComponents/Effects";
+
+const Sidebar = () => {
+  return (
+    <div className="sidebar">
+      <div className="sidebar-list-styling">
+        <ul className="sidebar-list list-unstyled">
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/resources">Resources</Link>
+          </li>
+          <li>
+            <Link to="/functionalcomponent">Functional Component</Link>
+          </li>
+          <li>
+            <Link to="/jsxrules">JSX RULES</Link>
+          </li>
+          <li>
+            <Link to="/state">State</Link>
+          </li>
+          <li>
+            <Link to="/effects">Effects</Link>
+          </li>
+        </ul>
+      </div>
+      <div className="sidebar-route">
+        <Switch>
+          <Route exact path="/home">
+            <Home />
+          </Route>
+          <Route exact path="/resources">
+            <Resources />
+          </Route>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route exact path="/functionalcomponent">
+            <FunctionalComponentDemo />
+          </Route>
+          <Route exact path="/jsxrules">
+            <JSXRules />
+          </Route>
+          <Route exact path="/state">
+            <State />
+          </Route>
+          <Route exact path="/effects">
+            <Effects />
+          </Route>
+        </Switch>
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
